85c49b757ca12692be4783af87a7c1ab14943c96
Fabio De Carvalho Almeida <fade-car@c1r9p4.42sp.org.br>
2025-08-26 11:02:56 -0300

    refactored ex08

--- ex08/transform.py
+++ ex08/transform.py
@@ -1,12 +1,13 @@
 import sys
 
 
-def square_even_numbers(lst: list[str]) -> list[int]:
-    return [int(x) ** 2 for x in lst if int(x) % 2 == 0]
+def square_even_numbers(lst: list[int]) -> list[int]:
+    """receive a list of integers and return a new list of the square of even numbers in the list"""
+    return [x**2 for x in lst if x % 2 == 0]
 
 
 if __name__ == "__main__":
     try:
-        print(square_even_numbers(sys.argv[1].split(" ")))
+        print(square_even_numbers(list(map(int, sys.argv[1].split(" ")))))
     except IndexError:
         print("missing argument")
